3b933c0427524cff938e23e163a63aa27f2b0226
Joe Salerno <joesalerno.js@gmail.com>
2025-05-04 15:16:32 -0400

    Fix?

--- test.py
+++ test.py
@@ -55,7 +55,7 @@ OPTUNA_TRIALS = 1000000 # Number of Optuna trials (increased for better search)
 OPTUNA_TIMEOUT = 60 * 60 * 24 # Timeout for Optuna trials (in seconds)
 OPTUNA_STUDY_NAME = "recursive_lgbm_tuning"
 # OPTUNA_DB = f"sqlite:///optuna_study_{OPTUNA_STUDY_NAME}.db"
-OPTUNA_DB = "postgresql://neondb_owner:npg_SfKxERaWw36V@ep-nameless-sound-a4u4lfp4-pooler.us-east-1.aws.neon.tech/optuna?sslmode=require"
+OPTUNA_DB = "postgresql://neondb_owner:npg_b9Jo7RhUgpSd@ep-proud-dust-a4fztafy-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
 
 SUBMISSION_FILE_PREFIX = "submission_recursive"
 SHAP_FILE_PREFIX = "shap_recursive"
